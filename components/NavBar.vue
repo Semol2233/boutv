6156b29509ee45757975d8fd9cc70c68f4d90414
tanzilislam <tanjilislam90@gmail.com>
2020-06-10 23:20:05 +0600

    update

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,27 +1,50 @@
 <template>
   <div>
-    <b-navbar class="mb-2" toggleable="lg" type="dark" variant="dark">
+    <b-navbar
+      class="my-custom-nav mb-2"
+      toggleable="sm"
+      type="light"
+      variant="light"
+    >
       <b-container>
         <b-navbar-brand href="#">
           <b-img
-            style="height:50px;"
-            :src="require('~/static/brand.jpeg')"
+            style="height:50px; width:70px;"
+            :src="require('~/static/brand.png')"
           ></b-img>
         </b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item href="#">Authority</b-nav-item>
-            <b-nav-item href="#">Results</b-nav-item>
-            <b-nav-item href="#">Academic Info</b-nav-item>
-            <b-nav-item href="#">News/Notice</b-nav-item>
-            <b-nav-item href="#">Schools</b-nav-item>
+          <b-navbar-nav v-if="!showInputbar">
+            <b-nav-item active href="#">বাংলাদেশ</b-nav-item>
+            <b-nav-item active href="#">বিশ্ব</b-nav-item>
+            <b-nav-item active href="#">খেলাধুলা</b-nav-item>
+            <b-nav-item active href="#">বিনোদন</b-nav-item>
+            <b-nav-item active href="#">ছবি</b-nav-item>
+            <b-nav-item active href="#">ভিডিও</b-nav-item>
           </b-navbar-nav>
+          <div v-else class="shadow-sm w-100">
+            <b-input-group>
+              <b-form-input></b-form-input>
+              <b-input-group-append>
+                <b-button variant="dark" size="md">
+                  Search
+                </b-button>
+              </b-input-group-append>
+            </b-input-group>
+          </div>
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
             <b-nav-form>
-              <b-button variant="dark" size="md"
+              <b-button
+                v-if="!showInputbar"
+                @click="showInput"
+                variant="light"
+                size="md"
                 ><b-icon icon="search"></b-icon
+              ></b-button>
+              <b-button v-else variant="light" size="md" @click="showNavItem"
+                ><b-icon icon="x" variant="dark"></b-icon
               ></b-button>
             </b-nav-form>
           </b-navbar-nav>
@@ -32,8 +55,32 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      showInputbar: false
+    };
+  },
+  methods: {
+    showInput() {
+      this.showInputbar = true;
+    },
+    showNavItem() {
+      this.showInputbar = false;
+    }
+  }
+};
 </script>
 
 <style>
+@media (min-width: 575.98px) and (min-width: 768px) and (min-width: 992px) and (min-width: 1200px) {
+  .my-custom-nav {
+    height: 60px;
+  }
+}
+@media (min-width: 786px) {
+  .my-custom-nav {
+    height: 60px;
+  }
+}
 </style>
